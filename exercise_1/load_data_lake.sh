efb8511a2f8ea25bcf8cf5889300e2de934dc096
root <root@ip-172-31-52-70.ec2.internal>
2015-10-02 04:11:42 +0000

    ass bash script

---
+++ exercise_1/load_data_lake.sh
@@ -0,0 +1,4 @@
+tail -n +2 'Hospital General Information.csv' > effective_care.csv
+tail -n +2 'Timely and Effective Care - Hospital.csv' > effective_care.csv
+tail -n +2 'Readmissions and Deaths - Hospital.csv' > readmissions.csv
+tail -n +2 'hvbp_hcahps_05_28_2015.csv' > survey_responses.csv
