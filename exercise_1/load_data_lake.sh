46664945507c4588b7d1a486479c1a31fcb0b755
kyle hamilton <kyleih@optonline.net>
2015-10-11 20:07:46 -0400

    fix load script

--- exercise_1/load_data_lake.sh
+++ exercise_1/load_data_lake.sh
@@ -1,28 +1,35 @@
+#!/bin/bash
+
+cd /data/w205
 mkdir hospitals
+chmod -R 777 hospitals
 cd hospitals/
 
 # get data
-wget -O hosptals.zip https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
-
+wget -O  hospitals.zip "https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip"
 
 # unzip raw data
 mkdir raw_data
 chmod -R 777 raw_data
-mv hosptals.zip raw_data/
-(cd raw_data/; unzip hosptals.zip)
+mv hospitals.zip raw_data/
+cd raw_data/
+unzip hospitals.zip
 
 
 # rename files and put a level up
+cd ../
 tail -n +2 raw_data/'Hospital General Information.csv' > hospitals.csv
 tail -n +2 raw_data/'Timely and Effective Care - Hospital.csv' > effective_care.csv
 tail -n +2 raw_data/'Readmissions and Deaths - Hospital.csv' > readmissions.csv
 tail -n +2 raw_data/'hvbp_hcahps_05_28_2015.csv' > survey_responses.csv
 
 echo 'files renamed'
-ls
+ls -al
 
 # make hadoop dirs
-echo "making hadoop dirs"
+
+echo "making hadoop dirs... please be patient"
+
 hdfs dfs -mkdir /user/w205/hospital_compare
 
 hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
