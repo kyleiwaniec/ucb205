ae43d4fa50d8d95f20b7ed3da6e76fb585fca623
root <root@ip-172-31-63-28.ec2.internal>
2015-12-05 15:57:17 +0000

    add files

--- exercise_2/Twittercredentials.py
+++ exercise_2/Twittercredentials.py
@@ -1,18 +1,9 @@
 import tweepy
 
-consumer_key = "YourConsumerkey";
-#eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
-
-
-consumer_secret = "YourConsumerSecret";
-#eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
-
-access_token = "YourAcessToken";
-#eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
-
-access_token_secret = "YourTokenSecret";
-#eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
-
+access_token = "408180916-lexLejjnitxJ9128aL42twu8pEB1CtSy1dCJtO5V"
+access_token_secret = "Ot3laTPsDueEOFnjzj4gEm4wJq0AkQAlQSZMydV9HMgOD"
+consumer_key = "9L9LLLYE6fc7jsHo1WjVj8I9f"
+consumer_secret = "GDMh9ZxOGbOLOVAqUDLlB3M7R4DWn4QnUFSa8rMmZmc2y3NUfo"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
