745190aa760189a46c3f7e98caaf12b026efb143
kyle hamilton <kyleih@optonline.net>
2015-12-06 21:47:46 -0500

    update install script

--- exercise_2/install-dependencies.sh
+++ exercise_2/install-dependencies.sh
@@ -10,7 +10,7 @@ if [[ "$answer" == "1" ]]; then
 	chmod a+x /usr/local/bin/lein
 	sudo /usr/local/bin/lein
 
-#	pip install matplotlib
+	pip install matplotlib
 	pip install streamparse
 	pip install psycopg2
 	pip install argparse
@@ -18,24 +18,31 @@ if [[ "$answer" == "1" ]]; then
 	pip install tweepy
 
 elif [[ "$answer" == "2" ]]; then
-	sudo yum install python27-devel –y
-	mv /usr/bin/python /usr/bin/python266
-	ln -s /usr/bin/python2.7 /usr/bin/python
-	sudo curl -o ez_setup.py https://bootstrap.pypa.io/ez_setup.py
-	sudo python ez_setup.py
-	sudo /usr/bin/easy_install-2.7 pip
-	sudo pip install virtualenv
+	# sudo yum install python27-devel –y
+	# mv /usr/bin/python /usr/bin/python266
+	# ln -s /usr/bin/python2.7 /usr/bin/python
+	# sudo curl -o ez_setup.py https://bootstrap.pypa.io/ez_setup.py
+	# sudo python ez_setup.py
+	# sudo /usr/bin/easy_install-2.7 pip
+	# sudo pip install virtualenv
 
-	wget --directory-prefix=/usr/bin/ https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
-	chmod a+x /usr/bin/lein
-	sudo /usr/bin/lein
+	# wget --directory-prefix=/usr/bin/ https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+	# chmod a+x /usr/bin/lein
+	# sudo /usr/bin/lein
 
-	mkdir /usr/local/lib/tmp_h/
-	mv /usr/local/lib/libpython2.7.a /usr/local/lib/tmp_h/
-	pip install --no-cache-dir matplotlib
-	mv /usr/local/lib/tmp_h/libpython2.7.a /usr/local/lib/
+	# mkdir /usr/local/lib/tmp_h/
+	# mv /usr/local/lib/libpython2.7.a /usr/local/lib/tmp_h/
+	# pip install --no-cache-dir matplotlib
+	# mv /usr/local/lib/tmp_h/libpython2.7.a /usr/local/lib/
 
-
+	wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+	python2.7 ez_setup.py
+	easy_install-2.7 pip
+	pip2.7 install virtualenv
+	virtualenv -p python2.7 27env
+	source 27env/bin/activate
+	
+	pip install matplotlib
 	pip install streamparse
 	pip install psycopg2
 	pip install argparse
